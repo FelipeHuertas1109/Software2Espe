cdaa3265ec8a9b7beb522d5a0f391e801f78c701
Felipe Huertas <fhuertas@unillanos.edu.co>
2025-09-27 11:50:08 -0500

    Integrate Bootstrap for styling, add Dragon Ball route, and update Navbar links; remove Car component files.

--- project26sep/src/app/app.routes.ts
+++ project26sep/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { CarComponent } from './car/car.component';
+import { CarComponent } from './pages/car/car.component';
+import { DragonBallComponent } from './pages/dragonball/dragonball';
 export const routes: Routes = [
   {
     path: 'car',
     component: CarComponent
+  },
+  {
+    path: 'dragonball',
+    component: DragonBallComponent
   },
   {
     path: '**',
