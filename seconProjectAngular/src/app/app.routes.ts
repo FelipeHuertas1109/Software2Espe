6737b79cc64d36d908db226b3e379fdea4a9a524
Felipe Huertas <fhuertas@unillanos.edu.co>
2025-09-26 21:32:28 -0500

    Updated calculations for tax processing

---
+++ seconProjectAngular/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CarComponent } from './pages/car/car.component';
+
+export const routes: Routes = [
+  {
+    path: 'car',
+    component: CarComponent
+  }
+];
